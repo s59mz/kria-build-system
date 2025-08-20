adb42b76e11cf20b37e4004ab3447d319bc9a115
Matjaz Zibert <you@example.com>
2025-08-20 21:54:03 +0200

    fixed CMA parameter in bootargs

--- boot/boot.cmd
+++ boot/boot.cmd
@@ -8,9 +8,9 @@ echo "Loading kernel image.fit..."
 load usb 0:1 0x10000000 image.fit
 
 echo "Setting bootargs..."
-#setenv bootargs 'console=ttyPS1,115200 root=/dev/sda2 rw rootwait earlycon ip=dhcp'
+setenv bootargs 'console=ttyPS1,115200 root=/dev/sda2 rw rootwait earlycon ip=dhcp cma=900M'
 
-setenv bootargs console=ttyPS1,115200 root=/dev/nfs rw rootwait nfsroot=192.168.1.100:/Kria-images/kria-build-system/rootfs,v3 ip=dhcp cma=900M init=/sbin/init
+# setenv bootargs console=ttyPS1,115200 root=/dev/nfs rw rootwait nfsroot=192.168.1.100:/Kria-images/kria-build-system/rootfs,v3 ip=dhcp cma=900M init=/sbin/init
 
 echo "Booting..."
 bootm 0x10000000#conf-smk-k26-revA-sck-kr-g-revB 0x10000000#conf-smk-k26-revA-sck-kr-g-revB 0x10000000#conf-smk-k26-revA-sck-kr-g-revB
